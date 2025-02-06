65710eba30ad61dc90662a34a07621e8f9db32b7
Felipe Montenegro <felipe.montenegro@idslatam.com>
2025-02-06 18:52:06 -0500

    -Creando proyecto de consola para testear código.

---
+++ vali-flow-test/Models/User.cs
@@ -0,0 +1,8 @@
+namespace vali_flow_test.Models;
+
+public class User
+{
+    public string Name { get; set; } = string.Empty;
+    public bool IsActive { get; set; }
+    public int Age { get; set; }
+}
---
+++ vali-flow-test/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using vali_flow_test.Models;
+using vali_flow.Builder;
+
+List<User> users = new List<User>
+{
+    new User { Name = "Alice", IsActive = true, Age = 25 },
+    new User { Name = "Bob", IsActive = false, Age = 17 },
+    new User { Name = "", IsActive = true, Age = 30 },
+    new User { Name = "Charlie", IsActive = true, Age = 20 }
+};
+
+var validator = new ValiFlow<User>()
+    .NotNull(x => x.Name)
+    .IsTrue(x => x.IsActive)
+    .GreaterThan(x => x.Age, 18);  // Asegura que la edad sea mayor a 18
+
+foreach (var user in users)
+{
+    var result = validator.Evaluate(user);
+    Console.WriteLine(result ? $"✅ {user.Name} es válido" : $"❌ {user.Name} no es válido");
+}
